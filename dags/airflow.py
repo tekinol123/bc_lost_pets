95e4b2d931df45703565f1f1fc98fa2152f5ca7c
Diego Hernandez <diego.hdz6263@gmail.com>
2023-04-04 21:23:17 -0700

    save progress

--- dags/airflow.py
+++ dags/airflow.py
@@ -1,5 +1,11 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 
-
+with DAG(
+    dag_id='hello_world',
+    description='Hello World DAG',
+    start_date=datetime(2023, 4, 4, 17),
+    schedule_interval='@daily',
+) as dag:
+    pass
 
