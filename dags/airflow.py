f61d3c8da8e95e7d917cabfffb0cbc372670312f
Diego Hernandez <diego.hdz6263@gmail.com>
2023-04-06 19:00:10 -0700

    airflow

--- dags/airflow.py
+++ dags/airflow.py
@@ -1,11 +1,26 @@
 from airflow import DAG
 from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def greet():
+    print('Hello World')
 
 with DAG(
     dag_id='hello_world',
+    default_args=default_args,
     description='Hello World DAG',
-    start_date=datetime(2023, 4, 4, 17),
+    start_date=datetime(2023, 4, 6),
     schedule_interval='@daily',
 ) as dag:
-    pass
+    task1 = PythonOperator(
+        task_id='greet',
+        python_callable=greet,
+    )
+    task1
 
